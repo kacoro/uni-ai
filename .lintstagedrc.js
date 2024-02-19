bb149ff332d8af2fd70cd66f30dd95d2a659740b
kacoro <kacoro@qq.com>
2024-02-20 07:46:08 +0800

    feat(app init): app init

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,15 +1,23 @@
-const { ESLint } = require('eslint');
+// const { ESLint } = require('eslint');
 
-const removeIgnoredFiles = async (files) => {
-  const eslint = new ESLint();
-  const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
-  const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
-  return filteredFiles.join(' ');
-};
+// const removeIgnoredFiles = async files => {
+//   const eslint = new ESLint();
+//   const ignoredFiles = await Promise.all(files.map(file => eslint.isPathIgnored(file)));
+//   const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+//   return filteredFiles.join(' ');
+// };
 
+// module.exports = {
+//   '*': async files => {
+//     const filesToLint = await removeIgnoredFiles(files);
+//     return [`eslint ${filesToLint} --max-warnings=0`];
+//   }
+// };
+/**
+ * lint-staged 校验暂存区的文件
+ */
 module.exports = {
-  '*': async (files) => {
-    const filesToLint = await removeIgnoredFiles(files);
-    return [`eslint ${filesToLint} --max-warnings=0`];
-  },
+  'src/**/*.{js,ts,tsx,jsx,vue}': ['pnpm lint:fix', 'prettier --write'],
+  'src/**/*.{css,less,scss}': ['prettier --write'],
+  'src/**/*.{md,json}': ['prettier --write']
 };
